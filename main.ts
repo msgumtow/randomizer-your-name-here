6f982851693bc6224c87c565a20c3b866dd2a56b
msgumtow <71787320+msgumtow@users.noreply.github.com>
2021-01-04 13:44:45 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+/**
+ * Decide what you're going to have as random choices! 
+ * 
+ * 1) Make a variable.
+ * 
+ * 2) Drag a red "set variable to" block into the input.
+ * 
+ * 3) In the math section, find the block that says "pick random" and replace the 0 in the set variable block. Choose the number of choices you want to have! Must be at least 2.
+ * 
+ * 4) Grab an "if true then else" conditional block and put it inside the input as well.  
+ * 
+ * 5) Drag the 0=0 block that's in the workspace to replace the "true" in the conditional block.
+ * 
+ * 6) Put a red, circular variable block to replace the first 0 after "if." Then, change the second 0 to be one of the numbers you chose to be randomly selected. 
+ * 
+ * 7) Underneath the "if variable = number, then" space, put in what you want to display. It could be words, symbols, or numbers.
+ * 
+ * 8) Do the same for under the "else" section.
+ * 
+ * 9) If you have more than 2 choices, click the plus sign under "else" to add more! You'll need to duplicate the "variable = number" block and put it in, then change the number.
+ * 
+ * Share, add your name to the file, copy the link and submit!
+ */
+input.onButtonPressed(Button.A, function () {
+	
+})
